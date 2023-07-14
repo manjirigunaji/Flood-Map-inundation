939f099039baad3ec9deaa07498b536b824c0b4c
dylanlee <dylanblee@gmail.com>
2023-07-14 12:08:56 -0500

    Update combineHUCextents.sql

--- combineHUCextents.sql
+++ combineHUCextents.sql
@@ -1,4 +1,4 @@
--- script that combines all the HAND extents you've imported
+-- combine the HUC8 extents you exported from the HAND FIM4 inundation mapping code
 DO $$ 
 DECLARE 
     tbl_name text;
@@ -9,12 +9,12 @@ BEGIN
     FOR tbl_name IN 
         SELECT table_name 
         FROM information_schema.tables 
-        WHERE table_schema = 'public'  -- replace <username> with your actual username
+        WHERE table_schema = 'public'
             AND table_type = 'BASE TABLE' 
             AND table_name LIKE 'hand_extent%' 
     LOOP
         EXECUTE format(
-            'INSERT INTO combined_handmap SELECT ST_Reclass(rast, 1, ''[-infinity-1):0, [1-infinity):1'', ''8BUI'', 0) FROM %I', 
+            'INSERT INTO combined_handmap SELECT ST_SetSRID(ST_Reclass(rast, 1, ''[-infinity-1):0, [1-infinity):1'', ''8BUI'', 0), 4326) FROM %I', 
             tbl_name
         );
     END LOOP;
